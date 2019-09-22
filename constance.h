9435d170c8ea2b815719c746919881cd83f1090a
Nguyen Nang Hung <47592733+HungNguyen20@users.noreply.github.com>
2019-09-22 21:39:50 +0700

    Add files via upload
    Đây là tất cả các file.h cần thiết để chạy main.c
    Chú ý rằng hãy tạo một thư mục "NotGAFunction" để lưu trữ file subfunction.h

---
+++ constance.h
@@ -0,0 +1,12 @@
+#define _GENE_LENGTH_ 10 // Số lượng gene trong chuỗi Chromosome.
+#define _POP_ 10 // Số lượng cá thế trong quần thể
+#define _PROBABILITY_CROSSOEVR_ 95  // Hằng số xác suất trao đổi chéo gene
+#define _PROBABILITY_MUTATION_ 20 // Hằng số xác suất  đột biến
+#define _TIME_TO_CHANGE_ 5 //Hằng số kết thúc vòng lặp
+#define _CAPACITY_ 30 //Hằng số khối lượng max mà túi chứa được
+
+/*
+Item:             1   2   3   4   5   6   7   8   9   10
+Khối lượng:       5   3   1   4   7   6   9   2   3   8
+Giá trị:          6   4   3   6   8   4   7   3   5   8
+*/
